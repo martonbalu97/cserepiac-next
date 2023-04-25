46850085a976623801f1056360128ef35c1bc294
martonbalu97 <113354730+martonbalu97@users.noreply.github.com>
2023-04-25 21:00:59 +0200

    Upload UI

--- app/components/modals/UploadModal.tsx
+++ app/components/modals/UploadModal.tsx
@@ -3,23 +3,81 @@
 import Modal from "./Modal";
 import useUploadModal from "@/app/hooks/useUploadModal"
 import TypeWriter from "@/app/typewriter/TypeWriter";
-import {useEffect,useState} from 'react'
+import {useEffect,useState, useMemo} from 'react'
+import Heading from "../Heading";
+import { categories } from "../navbar/Categories";
+import CategoryInput from "../inputs/CategoryInput";
+
+enum STEPS {
+    CATEGORY = 0,
+    SUBCATEGORY = 1,
+    PRODNAME = 2,
+    DESCRIPTION = 3,
+    IMAGES = 4
+}
 
 const UploadModal = () => {
     const uploadModal = useUploadModal();
-    const [text, setText] = useState("")
-    const bodyContent = (
-        <div>
-            <TypeWriter 
-         text={text}
-         interKeyStrokeDurationInMs={140}/>
-       </div>
-     )
-
-    useEffect(() =>{
-          setText("Hello Balázs, én foglak végigvezetni a kezdeteken...")
-    })
     
+    const [step,setSteps] = useState(STEPS.CATEGORY)
+
+    const onBack = () =>{
+        setSteps((value) => value - 1)
+    };
+
+    const onNext = () =>{
+        setSteps((value) => value + 1)
+    }
+
+    const actionLabel = useMemo(() =>{
+        if (step == STEPS.IMAGES){
+        return 'Termék feltöltése'
+        }
+
+        return 'Következő'
+    }, [step])
+
+    const secondaryActionLabel = useMemo(() =>{
+        if (step == STEPS.CATEGORY){
+        return undefined
+        }
+
+        return 'Vissza'
+    }, [step])
+
+    let bodyContent = (
+        <div
+        className="flex flex-col gap-8"
+        >
+            <Heading
+            title="Hova sorolnád a termékedet?"
+            subtitle="Válassz a kategóriák közül.."
+            />
+            <div
+            className="
+            grid
+            grid-cols-1
+            md:grid-cols-2
+            gap-3
+            max-h[50vh]
+            overflow-y-auto
+            "
+            >
+              {categories.map((item) =>(
+                <div
+                key={item.label}
+                >
+                   <CategoryInput
+                   onClick={() => {}}
+                   selected={false}
+                   label={item.label}
+                   icon={item.label}
+                   />
+                </div>
+              ))}
+            </div>
+        </div>
+    )
 
     return (
     <Modal
@@ -27,7 +85,9 @@ const UploadModal = () => {
     isOpen={uploadModal.isOpen}
     onClose={uploadModal.onClose}
     onSubmit={uploadModal.onClose}
-    actionLabel="Tovább"
+    actionLabel={actionLabel}
+    secondaryActionLabel={secondaryActionLabel}
+    secondaryAction={step == STEPS.CATEGORY ? undefined : onBack}
     body={bodyContent}
     />
     )
