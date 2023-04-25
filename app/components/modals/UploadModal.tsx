906766f01a280b650f73d9ae4db886eba26045db
martonbalu97 <113354730+martonbalu97@users.noreply.github.com>
2023-04-25 20:04:35 +0200

    dasd

--- app/components/modals/UploadModal.tsx
+++ app/components/modals/UploadModal.tsx
@@ -2,16 +2,26 @@
 
 import Modal from "./Modal";
 import useUploadModal from "@/app/hooks/useUploadModal"
+import TypeWriter from "@/app/typewriter/TypeWriter";
 
 const UploadModal = () => {
     const uploadModal = useUploadModal();
+
+    const bodyContent = (
+        <div>
+            <TypeWriter 
+         text={"Hello Balázs, én foglak végigvezetni a kezdeteken..."}
+         interKeyStrokeDurationInMs={160}/>
+        </div>
+    )
     return ( 
         <Modal
         title="Termék feltöltése"
         isOpen={uploadModal.isOpen}
         onClose={uploadModal.onClose}
         onSubmit={uploadModal.onClose}
-        actionLabel="Tovább"/>
+        actionLabel="Tovább"
+        />
      );
 }
  
