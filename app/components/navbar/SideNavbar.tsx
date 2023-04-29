80e407a55e7213cf8735615390583f4fa97d41fc
martonbalu97 <113354730+martonbalu97@users.noreply.github.com>
2023-04-29 21:55:27 +0200

    fsd

--- app/components/navbar/SideNavbar.tsx
+++ app/components/navbar/SideNavbar.tsx
@@ -103,7 +103,7 @@ const SideNavbar: React.FC<UserMenuProps> = ({
                 </h3>
               </div>
               <div className="flex mb-2 justify-start items-center gap-4 pl-4 hover:bg-gray-900 p-2 rounded-md group cursor-pointer hover:shadow-lg m-auto">
-                <MdOutlineSpaceDashboard className="text-2xl text-gray-600 group-hover:text-white " />
+                <MdOutlineSpaceDashboard size={30} className="text-2xl text-gray-600 group-hover:text-white " />
                 <h3 className="text-base text-gray-800 group-hover:text-white font-semibold ">
                   Dashboard
                 </h3>
@@ -137,7 +137,7 @@ const SideNavbar: React.FC<UserMenuProps> = ({
                     >
                     <MdOutlineLogout className="text-2xl text-gray-600 group-hover:text-white " />
                       <h3 className="text-base text-gray-800 group-hover:text-white font-semibold ">
-                        Regisztráció
+                        Kilépés
                       </h3>
                     </div>
                     </>
